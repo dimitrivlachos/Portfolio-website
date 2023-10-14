8159837d9a7b4cc3f1a34c9d74b33d24b80ae373
Dimitri Vlachos <86776716+dimitrivlachos@users.noreply.github.com>
2023-10-14 17:59:28 +0100

    Fixed white bars

--- src/components/NavigationComp.vue
+++ src/components/NavigationComp.vue
@@ -51,7 +51,7 @@
         <!-- Dropdown menu from item -->
         <div :class="{ 'hidden': !isSubMenuOpen, 'w-full': isSubMenuOpen }">
             <div
-                class="mt-1 border-gray-200 shadow-sm bg-gray-50 md:bg-white border-y dark:bg-gray-800 dark:border-gray-600">
+                class="mt-1 border-gray-200 shadow-sm bg-white border-y dark:bg-gray-800 dark:border-gray-600">
                 <div
                     class="bg-white dark:bg-gray-800 grid max-w-screen-xl px-4 py-5 mx-auto text-gray-900 dark:text-white sm:grid-cols-2 md:px-6">
                     <ul>
