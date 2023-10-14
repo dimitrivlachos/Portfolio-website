228ac15c52adcd39f1197f238f497db47355aa99
Dimitri Vlachos <86776716+dimitrivlachos@users.noreply.github.com>
2023-10-14 03:44:32 +0100

    Modifed Flowbite navbar to work in native vue

---
+++ src/components/NavComponents/DropdownMenuComp.vue
@@ -0,0 +1,16 @@
+<template>
+    <li>
+      <a :href="item.link" class="block p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700">
+        <div class="font-semibold">{{ item.title }}</div>
+        <span class="text-sm text-gray-500 dark:text-gray-400">{{ item.description }}</span>
+      </a>
+    </li>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      item: Object,
+    },
+  };
+  </script>
