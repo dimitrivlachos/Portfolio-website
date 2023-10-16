c5d79ae82831d13fda48a1f2c72fa70addb7ed6c
Dimitri Vlachos <86776716+dimitrivlachos@users.noreply.github.com>
2023-10-16 22:01:36 +0100

    Changed heading and title

--- src/components/NavigationComp.vue
+++ src/components/NavigationComp.vue
@@ -2,8 +2,8 @@
     <nav class="bg-white border-gray-200 dark:border-gray-600 dark:bg-gray-900">
         <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
             <a href="https://flowbite.com" class="flex items-center">
-                <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="Flowbite Logo" />
-                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+                <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="Logo" />
+                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Dimitri John Vlachos</span>
             </a>
             <!-- Hamburger button -->
             <button @click="toggleMenu"
