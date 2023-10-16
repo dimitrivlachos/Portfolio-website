1bcec94aa9e0319d16f2e797af560de31e934b51
Dimitri Vlachos <86776716+dimitrivlachos@users.noreply.github.com>
2023-10-17 00:36:16 +0100

    Created empty template for p5 sketches

---
+++ src/components/p5/p5Template.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="p5Canvas"></div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import P5 from 'p5'
+
+export default defineComponent({
+    mounted() {
+        let script = function (p5) {
+            var example = 3;
+ 
+            p5.setup = _ => {
+                
+            }
+
+            p5.draw = _ => {
+                
+            }
+        }
+
+        new P5(script)
+    }
+}
+)
+</script>
