f5899de58249f6e004391dd3242f5b96bdc071b1
Dimitri Vlachos <86776716+dimitrivlachos@users.noreply.github.com>
2023-10-17 00:11:54 +0100

    Test p5 component

---
+++ src/components/p5.vue
@@ -0,0 +1,44 @@
+<template>
+        <div id="p5Canvas"></div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import P5 from 'p5'
+
+export default defineComponent({
+    mounted() {
+        let script = function (p5) {
+            var speed = 2;
+            var posX = 0;
+
+            // NOTE: Set up is here   
+            p5.setup = _ => {
+                var canvas = p5.createCanvas(500, 500)
+                canvas.parent("p5Canvas");
+                p5.ellipse(p5.width / 2, p5.height / 2, 500, 500);
+            }
+
+            // NOTE: Draw is here
+            p5.draw = _ => {
+                p5.background(0);
+                const degree = p5.frameCount * 3;
+                const y = p5.sin(p5.radians(degree)) * 50;
+
+                p5.push();
+                p5.translate(0, p5.height / 2);
+                p5.ellipse(posX, y, 50, 50);
+                p5.pop();
+                posX += speed;
+
+                if (posX > p5.width || posX < 0) {
+                    speed *= -1;
+                }
+            }
+        }
+
+        new P5(script)
+    }
+}
+)
+</script>
