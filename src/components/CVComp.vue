52d25b44f2b5878ccbc43819a2108d089120f6c7
Dimitri Vlachos <86776716+dimitrivlachos@users.noreply.github.com>
2023-10-17 22:12:58 +0100

    Updated CV layout and details

---
+++ src/components/CVComp.vue
@@ -0,0 +1,76 @@
+<template>
+    <!-- Two-column section -->
+    <div class="grid md:grid-cols-3 lg:grid-cols-4 justify-center">
+
+        <!-- Spacer div -->
+        <div class="hidden lg:block lg:col-span-1 p-4">
+            <!-- Maybe add a profile picture here? -->
+        </div>
+
+        <!-- Main section -->
+        <div class="md:col-span-2 p-4 mx-auto lg:mx-0">
+            
+
+            <h2 class="text-2xl font-semibold">Profile</h2>
+            <p>Graduate Computer Scientist from the University of London with First Class Honours.
+            Strong foundation in Computer Science, Software Engineering, and data structures.
+            Demonstrated self-driven nature, completing my degree independently.
+            Possesses problem-solving mindset, analytical approach, and quick adaptability to new
+            technologies. Thrives in challenging projects and seeks to make meaningful
+            contributions to organizations. Seeking opportunities to further develop abilities and
+            contribute to innovative initiatives.</p>
+
+            <h2 class="text-2xl pt-6">Education</h2>
+            <h2>University of London</h2>
+        </div>
+
+        <!-- Sidebar section -->
+        <div class="col-span-1 p-4 mx-auto lg:mx-0">
+            <h1 class="text-2xl">Skills</h1>
+            <h2 class="text-xl">Programming Languages</h2>
+            <h3 class="text-lg">Proficient</h3>
+            <p>Python, C++, C#</p>
+            <h3 class="text-lg pt-2">Experienced</h3>
+            <p>Java, JavaScript, HTML, CSS, SQL</p>
+            <h3 class="text-lg pt-2">Familiar</h3>
+            <p>LATEX, RUST</p>
+
+            
+            <h2 class="text-xl pt-6">Frameworks</h2>
+            <ul>
+                <li>Vue.js</li>
+                <li>React</li>
+                <li>Node.js</li>
+                <li>Express</li>
+                <li>Flask</li>
+                <li>Bootstrap</li>
+                <li>Tailwind CSS</li>
+            </ul>
+
+            <h2 class="text-xl pt-6">Tools/Platforms</h2>
+            <ul>
+                <li>Git</li>
+                <li>GitHub</li>
+                <li>GitLab</li>
+                <li>Linux</li>
+                <li>Windows</li>
+                <li>macOS</li>
+                <li>Arduino</li>
+                <li>Raspberry Pi</li>
+            </ul>
+
+        </div>
+        
+    </div>
+</template>
+  
+<script>
+export default {
+    data() {
+        return {
+            
+        }
+    }
+};
+</script>
+  
--- src/views/CVView.vue
+++ src/views/CVView.vue
@@ -1,5 +1,32 @@
 <template>
-    <div>
-        <h1> CV View </h1>
+    <div class="text-black dark:text-slate-300">
+        <h1 class="text-center text-6xl p-8"><p class="inline font-semibold">Dimitrios </p><p class="inline font-thin">Vlachos</p></h1>
+
+        <!-- Social links -->
+        <div class="flex justify-center">
+            <a href="https://www.linkedin.com/in/dimitri-vlachos/" target="_blank" rel="noopener noreferrer" class="mx-4">
+                <LinkedInSVG width="40" height="40" class="inline"/><p class="inline font-thin pl-2">LinkedIn</p>
+            </a>
+            <a href="https://github.com/dimitrivlachos" target="_blank" rel="noopener noreferrer" class="mx-4">
+                <GitHubSVG width="40" height="40" class="inline"/><p class="inline font-thin pl-2">GitHub</p>
+            </a>
+        </div>
+
+        <CVComp />
     </div>
 </template>
+
+<script>
+    import CVComp from '../components/CVComp.vue';
+    import LinkedInSVG from '../assets/svg/LinkedInSVG.vue';
+    import GitHubSVG from '../assets/svg/GitHubSVG.vue';
+
+    export default {
+        name: 'CVView',
+        components: {
+            CVComp,
+            LinkedInSVG,
+            GitHubSVG
+        }
+    }
+</script>
