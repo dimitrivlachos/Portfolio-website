b39715ef792091ed6160aa8607963bb50cfb59fa
Dimitri Vlachos <86776716+dimitrivlachos@users.noreply.github.com>
2023-10-19 01:26:48 +0100

    Renamed CV to Resume

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
-import CVView from '../views/CVView.vue';
+import ResumeView from '../views/ResumeView.vue';
 import ContactView from '../views/ContactView.vue';
 import NotFoundView from '../views/NotFoundView.vue';
 
@@ -11,9 +11,9 @@ const routes = [
         component: HomeView,
     },
     {
-        path: '/cv',
-        name: 'CV',
-        component: CVView,
+        path: '/resume',
+        name: 'Resume',
+        component: ResumeView,
     },
     {
         path: '/portfolio',
--- src/views/ResumeView.vue
+++ src/views/ResumeView.vue
@@ -2,19 +2,19 @@
     <div class="text-black dark:text-slate-300">
         <h1 class="text-center text-6xl p-8"><p class="inline font-semibold">Dimitrios </p><p class="inline font-thin">Vlachos</p></h1>
 
-        <CVComp />
+        <ResumeComp />
     </div>
 </template>
 
 <script>
-    import CVComp from '../components/CVComp.vue';
+    import ResumeComp from '../components/ResumeComp.vue';
     import LinkedInSVG from '../assets/svg/LinkedInSVG.vue';
     import GitHubSVG from '../assets/svg/GitHubSVG.vue';
 
     export default {
-        name: 'CVView',
+        name: 'ResumeView',
         components: {
-            CVComp,
+            ResumeComp,
             LinkedInSVG,
             GitHubSVG
         }
